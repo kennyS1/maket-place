319a151209d4e2c49ab2d9d9fdcd0b54f95e9bba
Double New <3110648619@qq.com>
2025-04-11 17:11:28 +0800

    modify avater file location

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     phone = models.CharField(max_length=20, blank=True, null=True)  # 电话号码
-    avatar = models.ImageField(upload_to='media/accounts/', blank=True, null=True)  # 头像
+    avatar = models.ImageField(upload_to='accounts/', blank=True, null=True)  # 头像
     location = models.CharField(max_length=40, blank=True, null=True)  # 电话号码
     verified = models.BooleanField(default=False)   # 是否已验证
     
